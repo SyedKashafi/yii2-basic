60762400f40860c9129aec0646450745f4f4e2ce
SyedKashafi <syedkasha96@gmail.com>
2021-03-07 23:26:33 +0800

    db

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2 basic',
     'username' => 'root',
-    'password' => 'syed123',
+    'password' => 'sa',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
